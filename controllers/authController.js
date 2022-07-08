9abd50dc15b3a7e8953b8a6edda2696101aab523
Rue <ltsuyun1999@gmail.com>
2022-07-09 00:45:34 +0800

    add validator & modifies the auth pages

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,23 @@
+// import Schema
+const User = require('../models/User');
+
 module.exports.signup_get = (req,res)=>{
     res.render('auth/signup')
 }
 module.exports.login_get = (req,res)=>{
     res.render('auth/login')
 }
-module.exports.signup_post = (req,res)=>{
+module.exports.signup_post = async (req,res)=>{
     const { email, password } = req.body;
-    console.log(email)
-    console.log(password)
-    res.send('new signup')
+    
+    try{
+        const user = await User.create({ email, password });
+        res.status(201).json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('error, user not created.');
+    }
 }
 module.exports.login_post = (req,res)=>{
     const { email, password } = req.body;
